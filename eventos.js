494fa3bc2f91a803b00704f044dc4b23363eb0c5
gabriel falero <gabrielfalero96@gmail.com>
2023-09-01 13:15:13 -0300

    Create eventos.js

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// nos referimos al div y al boton
+const botonDiv = document.getElementById('botonDiv');
+
+// hace el trigger del evento click
+botonDiv.addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
